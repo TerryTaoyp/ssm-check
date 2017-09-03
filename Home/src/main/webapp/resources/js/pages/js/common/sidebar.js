3dcb92419840eedebcef94312e63dba647c9c6f4
wuxiaoyang <15290491277@163.com>
2017-09-03 20:59:36 +0800

    添加当前时间戳

--- Home/src/main/webapp/resources/js/pages/js/common/sidebar.js
+++ Home/src/main/webapp/resources/js/pages/js/common/sidebar.js
@@ -71,4 +71,28 @@ $(document).ready(function() {
 		// 		}
 		// 	});
 		// }
+		// 先执行一次获取当前时间
+        getNowFormatDate();
+		function getNowFormatDate() {
+		    var time=new Date();  
+          	var year=time.getFullYear();  //获取年份  
+          	var month=time.getMonth()+1;  //获取月份  
+          	var day=time.getDate();   //获取日期  
+          	var hour=checkTime(time.getHours());   //获取时  
+          	var minite=checkTime(time.getMinutes());  //获取分  
+          	var second=checkTime(time.getSeconds());  //获取秒  
+          	/****当时、分、秒、小于10时，则添加0****/  
+          	function checkTime(i){  
+          	    if(i<10) return "0"+i;  
+          	    return i;  
+          	} 
+          	var currentdate = year + "年" + month + "月" + day
+          	        + "日 " + hour + ":" + minite
+          	        + ":" + second;
+          	$('#nowTime').text(currentdate);
+		}
+		var nowTime = setInterval(function () {
+			getNowFormatDate();
+		},1000);
+
 });
